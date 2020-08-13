49f8b125c776e0c355ffe7dfe4bfd56ca997023a
Matt Sirkis <sirkis.matt@gmail.com>
2020-08-13 21:59:43 +1200

    initial commit

---
+++ src/List.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Cards from './Card'
+import './List.css';
+
+function list(props) {
+    return (
+        <section className="List">
+            <header className="List-header">
+                <h2>{props.header}</h2>
+            </header>
+            <div className="List-cards">
+                {props.cards.map((card) =>
+                    <Cards
+                    key={card.id}
+                    title={card.title}
+                    content={card.content}
+                    />
+                )}
+                <button
+                    type="button"
+                    className="List-add-button"
+                >
+                    + Add Random Card
+                </button>
+            </div>
+        </section>
+    )
+}
+
+export default list
